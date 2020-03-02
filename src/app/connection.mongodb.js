1b2009a31886499ef7007c37004e5e600de7faf1
Ravi Dubey <M1043165@mindtree.com>
2020-03-03 00:26:16 +0530

    MongoDB Configuration set bback to local connetion

--- src/app/connection.mongodb.js
+++ src/app/connection.mongodb.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const MONGODB_CONFIG = require('./config/mongodb.config');
 
 function connectMongoDB() {
-    const MONGO_ENDPOINT = 'mongodb://' + MONGODB_CONFIG['DB_USER'] + ':' + MONGODB_CONFIG['PASS'] + '@' + MONGODB_CONFIG['DB_URL'] + ":" + MONGODB_CONFIG['PORT'] + "/" + MONGODB_CONFIG['DB_NAME'];
+    // const MONGO_ENDPOINT = 'mongodb://' + MONGODB_CONFIG['DB_USER'] + ':' + MONGODB_CONFIG['PASS'] + '@' + MONGODB_CONFIG['DB_URL'] + ":" + MONGODB_CONFIG['PORT'] + "/" + MONGODB_CONFIG['DB_NAME'];
+    const MONGO_ENDPOINT = 'mongodb://' + MONGODB_CONFIG['DB_URL'] + ":" + MONGODB_CONFIG['PORT'] + "/" + MONGODB_CONFIG['DB_NAME'];
     const connect = mongoose.connect(MONGO_ENDPOINT, {
         useNewUrlParser: true,
         useUnifiedTopology: true
